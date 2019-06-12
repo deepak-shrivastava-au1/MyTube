c2b96f704cd03649b723ee0e3c38ead1e8d17bdc
Deepak Shrivastava <dpkshrivastava10@gmail.com>
2019-06-12 11:44:04 +0530

    Added spinner for  video loading

--- src/component/videos.js
+++ src/component/videos.js
@@ -7,9 +7,14 @@ class VideosComponent extends  React.Component {
 
     renderVideos() {
         return this.props.videos.map(v => {
+
+            let videoId = v.id;
+            if(typeof videoId != "string") {
+                videoId = v.id.videoId;
+            }
                 return (
                     <div key={v.etag}  className="col-md-4">
-                    <a target = "blank" href = {`http://youtube.com/watch?v=${v.etag}`}>
+                    <a target = "blank" href = {`http://youtube.com/watch?v=${videoId}`}>
                     <img className= "img-fluid" src={v.snippet.thumbnails.high.url} alt={v.snippet.title} />
                     </a>
                     <small>{v.snippet.title} by <em>{v.snippet.channelTitle}</em></small>
@@ -20,11 +25,22 @@ class VideosComponent extends  React.Component {
     }
  
     render() {
-       return (
-           <div className = "row">
-               {this.renderVideos()}
-           </div>
-       )
+        if(this.props.isVideoLoading) {
+            return (
+                <div className="d-flex justify-content-center">
+                     <div className="spinner-border text-danger" role="status">
+                         <span className="sr-only">Loading...</span> 
+                     </div>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className = "row">
+                    {this.renderVideos()}
+                </div>
+            )
+        } 
     }
 }
 let Videos = connect(stateMapper)(VideosComponent);
