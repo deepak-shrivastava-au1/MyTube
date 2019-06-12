3a2eb94cad78793f3c826ef2f3a90eb623f67995
Deepak Shrivastava <dpkshrivastava10@gmail.com>
2019-06-12 10:34:02 +0530

    isVideoLoading

---
+++ src/store/reducers/isVideoLoadingReducer.js
@@ -0,0 +1,18 @@
+import {fetchVideos} from '../api/youtube.js';
+ 
+
+function isVideoLoadingReducer(isVideoLoading = false, action) {
+
+    if(action.type === "FETCH_VIDEOS") {
+        
+        return true;
+    }
+
+    if(action.type === "VIDEOS_LOADED") {
+        
+        return false;
+    }
+    return isVideoLoading;
+  
+}
+export default isVideoLoadingReducer;
